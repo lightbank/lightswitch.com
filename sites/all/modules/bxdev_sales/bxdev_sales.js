e313b2a6457ee9d8505f0ed1d9ea11e038f2e41f
Steven Wanderski <steven.wanderksi@gmail.com>
2011-12-09 18:56:50 -0800

    add client add project sales pages display - no submit handlers

---
+++ sites/all/modules/bxdev_sales/bxdev_sales.js
@@ -0,0 +1,40 @@
+(function ($) {
+  
+	$(function(){
+		
+		if(window.location.hash = '#existing'){
+			$('.client-add-form').hide();
+			$('.client-existing-form').show();
+		}
+		
+		$('.show-client-add-form').click(function(){
+			$('.client-add-form').show();
+			$('.client-existing-form').hide();
+		});
+		
+		$('.show-client-existing-form').click(function(){
+			$('.client-add-form').hide();
+			$('.client-existing-form').show();
+		});
+		
+		
+		Drupal.jsAC.prototype.hidePopup = function (keycode) {
+			var selected_value = $(this.selected).text();
+			var nid = $(this.selected).data('autocompleteValue');
+			$('input[name="existing_client_nid"]').val(nid);
+			$('#edit-existing-client').val(selected_value);
+			
+			// Hide popup.
+			var popup = this.popup;
+			if (popup) {
+			  this.popup = null;
+			  $(popup).fadeOut('fast', function () { $(popup).remove(); });
+			}
+			this.selected = false;
+			$(this.ariaLive).empty();			
+		}
+		
+	});	
+
+}(jQuery));
+
