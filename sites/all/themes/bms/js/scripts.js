d7046dc3df958ae24d4436ee114fde36d975c94d
Steven Wanderski <steven.wanderksi@gmail.com>
2011-12-26 07:07:42 -0800

    business profile slider complete

--- sites/all/themes/bms/js/scripts.js
+++ sites/all/themes/bms/js/scripts.js
@@ -4,18 +4,41 @@
 		
 		Shadowbox.init();
 		
-		// video / image slider
-		$('.main .video-wrap-inner').bxSlider({
+		/**
+		 * Business profile
+		 */
+		
+		// video / image slider init
+		var business_slider = $('.main .video-wrap-inner').bxSlider({
 			mode: 'fade',
 			controls: false
 		});
 		
-		// thumb video / image slider
+		// thumb video / image slider init
 		$('.main .photo-thumbs-wrap-inner').bxSlider({
 			displaySlideQty: 4,
 			moveSlideQty: 4
 		});
 		
+		// click an image thumb
+		$('.video-thumbs-wrap .slide.pager').click(function(){
+			var index = $('.video-thumbs-wrap .slide.pager').index(this) + 1;
+			business_slider.goToSlide(index);
+			setTimeout(function(){
+				var video_content = $('#media-vimeo-1').clone().html();
+				$('#media-vimeo-1').html(video_content);
+			}, 500);
+		});
+		
+		// click the video thumb
+		$('.field-name-field-project-video img').click(function(){
+			business_slider.goToSlide(0);
+		});
+		
+		
+		/**
+		 * Project profile
+		 */				
 		
 		// comment textarea focus / blur
 		$('#edit-field-comment-und-0-value').focus(function() {
