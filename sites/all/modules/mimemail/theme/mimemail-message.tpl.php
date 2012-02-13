2848757d371a3da4b00abdbc7d093e4cd3618743
Steven Wanderski <steven.wanderksi@gmail.com>
2012-02-13 09:04:36 -0800

    html email design

--- sites/all/modules/mimemail/theme/mimemail-message.tpl.php
+++ sites/all/modules/mimemail/theme/mimemail-message.tpl.php
@@ -23,13 +23,23 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <?php if ($css): ?>
     <style type="text/css">
-      <!--
-      <?php print $css ?>
-      -->
+			
+			body {
+				padding: 20px;
+			}
+			
+			#main {
+				background: #f3f3f3;
+				padding: 20px 40px 30px;
+				font-size: 14px;
+				line-height: 18px;
+			}
+			
     </style>
     <?php endif; ?>
   </head>
   <body id="mimemail-body" <?php if ($key): print 'class="'. $key .'"'; endif; ?> style="padding: 20px;">
+		<div id="header"><img src="http://lightswitch.com/sites/default/files/mail_logo.jpg" /></div>
     <div id="center">
       <div id="main">
         <?php print $body ?>
