90d8683243180e703bc92d98d4c1b581afa756e1
Steven Wanderski <steven@bxcreative.com>
2012-02-14 17:42:45 -0800

    live cc

--- sites/all/modules/bxdev_payment/AuthnetCIM.class.php
+++ sites/all/modules/bxdev_payment/AuthnetCIM.class.php
@@ -33,7 +33,7 @@ class AuthnetCIMException extends Exception {}
 
 class AuthnetCIM
 {
-    const USE_PRODUCTION_SERVER  = 1;
+    const USE_PRODUCTION_SERVER  = 0;
     const USE_DEVELOPMENT_SERVER = 1;
 
     const EXCEPTION_CURL = 10;
@@ -633,6 +633,8 @@ class AuthnetCIM
     {
         $response = str_replace('xmlns="AnetApi/xml/v1/schema/AnetApiSchema.xsd"', '', $this->response);
         $xml = new SimpleXMLElement($response);
+
+				dpm($xml);
 
 				$this->responseXml = $xml;
 
