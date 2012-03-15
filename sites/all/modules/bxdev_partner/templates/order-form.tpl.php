4b3552431c4ae8233d0698e1d404f91a12e1a0a0
Steven Wanderski <steven.wanderksi@gmail.com>
2012-03-15 15:41:24 -0700

    partner portal live push

--- sites/all/modules/bxdev_partner/templates/order-form.tpl.php
+++ sites/all/modules/bxdev_partner/templates/order-form.tpl.php
@@ -17,13 +17,13 @@
 
 			<div class="greenArrowHead">
 				<div class="formFieldContainer">
-					<?php print render($form['city']); ?>
+					<?php print render($form['shoot_date']); ?>
 				</div>
 			</div><!--closes greenarrowbox -->
-
+			
 			<div class="greenArrowHead">
 				<div class="formFieldContainer">
-					<?php print render($form['shoot_date']); ?>
+					<?php print render($form['shoot_time']); ?>
 				</div>
 			</div><!--closes greenarrowbox -->
 
@@ -42,20 +42,26 @@
 					<?php print render($form['email']); ?>
 				</div>
 			</div><!--closes greenarrowbox -->
-
+			
 			<div class="greenArrowHead">
+				<div class="formFieldContainer">
+					<?php print render($form['city']); ?>
+					<a class="manual-city-link" href="">My city is not in the list</a>
+					<?php print render($form['city_manual']); ?>
+				</div>
+			</div><!--closes greenarrowbox -->
+
+			<?php /* ?>
+			<div class="greenArrowHead input-state">
 				<div class="formFieldContainer">
 					<?php print render($form['state']); ?>
 				</div>
 			</div><!--closes greenarrowbox -->
-
-			<div class="greenArrowHead">
-				<div class="formFieldContainer">
-					<?php print render($form['shoot_time']); ?>
-				</div>
-			</div><!--closes greenarrowbox -->
+			<?php */?>
 
 		</div><!--closes right column form Container -->
+		
+		<div class="clear"></div>
 
 	</div><!--closes innerSpotlight Container -->
 </div><!--closes large Gradient bar -->
@@ -64,4 +70,5 @@
   <?php print render($form['submit']); ?>
 </div>
 
+<?php unset($form['state']); ?>
 <?php print drupal_render_children($form); ?> 
--- sites/all/themes/partner/js/scripts.js
+++ sites/all/themes/partner/js/scripts.js
@@ -4,6 +4,14 @@
 		
 		$('#edit-shoot-date').datepicker();
 		
+		// phone number formatting
+		$('#edit-phone').mask('999-999-9999? x99999');
+		
+		$('.manual-city-link').click(function(){
+			$('.manual-city-field').slideToggle(200);
+			return false;
+		});
+		
 	});
 
 
