38d968d20b641e190441186045c7f891b38c8c95
Steven Wanderski <steven.wanderksi@gmail.com>
2012-04-23 07:31:40 -0700

    hide view photos from videog

--- sites/all/themes/bms/templates/node--project.tpl.php
+++ sites/all/themes/bms/templates/node--project.tpl.php
@@ -94,7 +94,6 @@
 			<?php if(isset($photos_teaser)): ?>
 				<div class="project-thumb-wrapper clearfix">
 					<?php print $photos_teaser; ?>
-					<div><a href="/project/<?php print arg(1); ?>/photos">View photos</a></div>
 				</div>
 			<?php endif; ?>
 		</div><!-- end .video -->
